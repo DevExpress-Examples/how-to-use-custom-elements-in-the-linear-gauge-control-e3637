37b185f08dd92750eadae3e4d770e716a64a7a4d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/LinearGaugeControl_CustomElement/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace LinearGaugeControl_CustomElement {
+    public partial class MainPage : UserControl {
+        public MainPage() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, System.Windows.RoutedEventArgs e) {
+            if (bar.Value < 100)
+                bar.Value += 10;
+        }
+
+        private void button2_Click(object sender, System.Windows.RoutedEventArgs e) {
+            if (bar.Value > 0)
+                bar.Value -= 10;
+        }
+    }
+}
